08ec5d325b042a248f72579ae87478e2bd721a9e
Rafaat Hossain <hossain@adobe.com>
2017-03-14 00:38:29 -0400

    fixing typo on ex2

--- app/models.py
+++ app/models.py
@@ -1,9 +1,9 @@
 from __future__ import unicode_literals
 from django.db import models
 from . import utils
-from . import aam.py
+from . import aam
 
-aam.get_self_user()
+aam.get_self()
 
 
 class Category(models.Model):
